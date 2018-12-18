c106f3e122fa44e3f9e009e1cdec09f8b93e9459
lluc <leger.luc@gmail.com>
2018-12-18 17:32:42 +0100

    [collection] supression des tables reliées.

--- telemeta_mshs/apps/telemeta_api/views/collection.py
+++ telemeta_mshs/apps/telemeta_api/views/collection.py
@@ -6,6 +6,14 @@
 
 from rest_framework import viewsets
 from ..models.collection import Collection as CollectionModel
+from ..models.collectioncollectors import CollectionCollectors
+from ..models.collection_informer import CollectionInformer
+from ..models.collection_location import CollectionLocation
+from ..models.collection_language import CollectionLanguage
+from ..models.collection_publisher import CollectionPublisher
+from ..models.performance_collection import PerformanceCollection
+from ..models.performance_collection_musician import (
+    PerformanceCollectionMusician)
 from ..serializers.collection import CollectionSerializer
 
 
@@ -16,3 +24,48 @@ class CollectionViewSet(viewsets.ModelViewSet):
 
     queryset = CollectionModel.objects.all()
     serializer_class = CollectionSerializer
+
+    def perform_destroy(self, instance):
+
+        # Delete the collectors
+        collectors = CollectionCollectors.objects.filter(
+            collection=instance.id)
+        for collector in collectors:
+            collector.delete()
+
+        # Delete the informers
+        informers = CollectionInformer.objects.filter(
+            collection=instance.id)
+        for informer in informers:
+            informer.delete()
+
+        # Delete the locations
+        locations = CollectionLocation.objects.filter(
+            collection=instance.id)
+        for location in locations:
+            location.delete()
+
+        # Delete the languages
+        languages = CollectionLanguage.objects.filter(
+            collection=instance.id)
+        for language in languages:
+            language.delete()
+
+        # Delete the publishers
+        publishers = CollectionPublisher.objects.filter(
+            collection=instance.id)
+        for publisher in publishers:
+            publisher.delete()
+
+        # Delete the performances
+        performances = PerformanceCollection.objects.filter(
+            collection=instance.id)
+        for performance in performances:
+            # Delete the musicians
+            musicians = PerformanceCollectionMusician.objects.filter(
+                performance_collection=performance.id)
+            for musician in musicians:
+                musician.delete()
+            performance.delete()
+
+        instance.delete()
