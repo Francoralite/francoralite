512cfc5218e2fab6a10100380eb92b05cdcfec6f
root <root@cchum-kvm-francoralite.in2p3.fr>
2019-07-24 10:53:02 +0200

    Delete networks 'service'. Working deployment

--- telemeta_mshs/settings/base.py
+++ telemeta_mshs/settings/base.py
@@ -74,7 +74,7 @@ DATABASES = {
     }
 }
 
-FRONT_HOST_URL = 'http://localhost:8000'
+FRONT_HOST_URL = 'http://app-francoralite-test.huma-num.fr:8000'
 USE_X_FORWARDED_HOST = True
 
 REST_FRAMEWORK = {
@@ -97,7 +97,7 @@ KEYCLOAK_BEARER_AUTHENTICATION_EXEMPT_PATHS = [
     'admin',
 ]
 KEYCLOAK_RSA_PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApPuhTYR5350AMvQnCIZJSxjlD2NiSAEi05HTRMlK3/B1IjGwfg6lP+LXrhLHRXWg7L+EzIU9oiuPqzHr/pfdWor6fPiLpII6fkmBuB18SiM7cm3xe7fyfjwhN/q0Cu1ddSo1ILR0O6/bXWCV5DHdNAdtnSQRWdnRbJCSkjzJ9/KrArvcAUx1k1bCBQ8nJ60tNRZbmLrC+1rHe8+9m9b3NpI2IRkpfJynfuxcpDAQiJ3orW8Hy3g1dgueGsiS6FWnLF9zJc5JuZQVGFukpKcl7COPp2CSEwxgPxjyZQ4GBynCfuvoNHogTEbeujE/erRM3i5VD5C5miUXz/cYiaKmqwIDAQAB
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhHDfEYJu9eS9ZQPgbwz8dVVpdHp1aaco0yOvKTVpjWfCWQpXqqDASrh4j7ycbiR0Vbgsvz3UPOXh4KgglpTLjN++3wIQ1xfiBfqxBdfj7otZy8JmbfhI5VyxUK4Gr7xrZcZ0qC64GS6ZgiXx1iHiupp/Ivn9ac7jO0jAkwqV9pxOsTqMFWQJ41IQnFRDWyIVLjgKLz5ybaoiJ4p9W3RUiDHigd6hW6ne+VlDLt0GjxilCcrL4JngszYYygnalJKUsgXM28NHNj8hco1FCHfQtHemKc+ck8fYlNkdS/sqNUNF+pBjfIRsCCX4hjwtvVH0T5gNDjt6Ar+junXMFLu5ywIDAQAB
 -----END PUBLIC KEY-----"""
 KEYCLOAK_CONFIG = {
     'KEYCLOAK_REALM': os.getenv('KEYCLOAK_REALM', 'francoralite'),
@@ -107,9 +107,9 @@ KEYCLOAK_CONFIG = {
         'KEYCLOAK_AUTHORIZATION_CONFIG', '/tmp/authorization_config.json'),
     'KEYCLOAK_METHOD_VALIDATE_TOKEN': 'DECODE',
     'KEYCLOAK_SERVER_URL': os.getenv(
-        'KEYCLOAK_SERVER_URL', 'http://keycloak.francoralite.localhost:8080/auth/'),
+        'KEYCLOAK_SERVER_URL', 'http://auth-francoralite-test.huma-num.fr:8080/auth/'),
     'KEYCLOAK_CLIENT_SECRET_KEY': os.getenv(
-        'KEYCLOAK_CLIENT_SECRET_KEY', 'ade263c9-5aca-46d4-965f-6e2d188a2515'),
+        'KEYCLOAK_CLIENT_SECRET_KEY', '76c8ea71-a26d-4ba8-a5f9-1411dfdf684b'),
     'KEYCLOAK_CLIENT_PUBLIC_KEY': KEYCLOAK_RSA_PUBLIC_KEY,
 }
 
@@ -119,7 +119,7 @@ KEYCLOAK_CONFIG = {
 #
 CORS_ORIGIN_WHITELIST = (
     os.getenv('API_EXTERNAL_IP_ADDRESS', '1.2.3.4'),
-    os.getenv('API_APP_EXTERNAL_URL', 'nginx.francoralite.localhost'),
+    os.getenv('API_APP_EXTERNAL_URL', 'app-francoralite-test.huma-num.fr'),
 )
 
 AUTHENTICATION_BACKENDS = (
@@ -128,23 +128,24 @@ AUTHENTICATION_BACKENDS = (
 OIDC_RP_SIGN_ALGO = "RS256"
 OIDC_RP_CLIENT_ID = os.getenv('KEYCLOAK_CLIENT_ID', 'francoralite')
 OIDC_RP_CLIENT_SECRET = os.getenv(
-    'KEYCLOAK_CLIENT_SECRET_KEY', 'ade263c9-5aca-46d4-965f-6e2d188a2515')
+    'KEYCLOAK_CLIENT_SECRET_KEY', '76c8ea71-a26d-4ba8-a5f9-1411dfdf684b')
 OIDC_CREATE_USER = True
 
 OIDC_OP_AUTHORIZATION_ENDPOINT = os.getenv(
-    'KEYCLOAK_SERVER_URL', 'http://keycloak.francoralite.localhost:8080/auth/') +\
+    'KEYCLOAK_SERVER_URL', 'https://auth-francoralite-test.huma-num.fr/auth/') +\
     "realms/francoralite/protocol/openid-connect/auth"
 OIDC_OP_TOKEN_ENDPOINT = os.getenv(
-    'KEYCLOAK_SERVER_URL', 'http://keycloak.francoralite.localhost:8080/auth/') +\
+    'KEYCLOAK_SERVER_URL', 'https://auth-francoralite-test.huma-num.fr/auth/') +\
     "realms/francoralite/protocol/openid-connect/token"
 OIDC_OP_USER_ENDPOINT = os.getenv(
-    'KEYCLOAK_SERVER_URL', 'http://keycloak.francoralite.localhost:8080/auth/') +\
+    'KEYCLOAK_SERVER_URL', 'https://auth-francoralite-test.huma-num.fr/auth/') +\
     "realms/francoralite/protocol/openid-connect/userinfo"
 OIDC_OP_JWKS_ENDPOINT = os.getenv(
-    'KEYCLOAK_SERVER_URL', 'http://keycloak.francoralite.localhost:8080/auth/') +\
+    'KEYCLOAK_SERVER_URL', 'https://auth-francoralite-test.huma-num.fr/auth/') +\
     "realms/francoralite/protocol/openid-connect/certs"
 
-LOGIN_REDIRECT_URL = "http://nginx.francoralite.localhost:8080/"
+LOGIN_REDIRECT_URL = "https://app-francoralite-test.huma-num.fr/"
+OIDC_REDIRECT_REQUIRE_HTTPS = True
 LOGOUT_REDIRECT_URL = os.getenv(
-    'KEYCLOAK_SERVER_URL', 'http://keycloak.francoralite.localhost:8080/auth/') +\
+    'KEYCLOAK_SERVER_URL', 'https://auth-francoralite-test.huma-numfr/auth/') +\
     "realms/francoralite/protocol/openid-connect/logout"
