ae58f5c56eaa700019fa51f660f3428539a62ed1
lluc <leger.luc@gmail.com>
2019-08-06 18:00:22 +0200

    [back] FIX migrations.

--- telemeta_mshs/apps/telemeta_api/migrations/0065_auto_20190806_1727.py
+++ telemeta_mshs/apps/telemeta_api/migrations/0065_auto_20190806_1727.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import datetime
+import django.core.validators
 
 
 class Migration(migrations.Migration):
@@ -14,6 +16,10 @@ class Migration(migrations.Migration):
         migrations.AlterModelOptions(
             name='acquisitionmode',
             options={'ordering': ['name'], 'verbose_name_plural': 'acquisitions'},
+        ),
+        migrations.AlterModelOptions(
+            name='collection',
+            options={'ordering': ['code', 'title'], 'verbose_name_plural': 'collections'},
         ),
         migrations.AlterModelOptions(
             name='coupe',
@@ -44,6 +50,14 @@ class Migration(migrations.Migration):
             name='value',
         ),
         migrations.RemoveField(
+            model_name='collection',
+            name='descriptions',
+        ),
+        migrations.RemoveField(
+            model_name='collection',
+            name='public_access',
+        ),
+        migrations.RemoveField(
             model_name='coupe',
             name='value',
         ),
@@ -70,49 +84,64 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='acquisitionmode',
             name='name',
-            field=models.CharField(default=b'2019-08-02 19:16:46.442534', unique=True, max_length=255, verbose_name='Nom'),
+            field=models.CharField(default=datetime.datetime(2019, 8, 6, 17, 26, 50, 731017), unique=True, max_length=255, verbose_name='Nom'),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='coupe',
             name='name',
-            field=models.CharField(default=b'2019-08-02 19:16:52.288286', unique=True, max_length=255, verbose_name='Nom'),
+            field=models.CharField(default=datetime.datetime(2019, 8, 6, 17, 27, 2, 228901), unique=True, max_length=255, verbose_name='Nom'),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='legalrights',
             name='name',
-            field=models.CharField(default=b'2019-08-02 19:17:00.769462', unique=True, max_length=255, verbose_name='Nom'),
+            field=models.CharField(default=datetime.datetime(2019, 8, 6, 17, 27, 8, 621821), unique=True, max_length=255, verbose_name='Nom'),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='mediatype',
             name='name',
-            field=models.CharField(default=b'2019-08-02 19:17:08.602626', unique=True, max_length=255, verbose_name='Nom'),
+            field=models.CharField(default=datetime.datetime(2019, 8, 6, 17, 27, 12, 614349), unique=True, max_length=255, verbose_name='Nom'),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='metadataauthor',
             name='name',
-            field=models.CharField(default=b'2019-08-02 19:17:14.875726', unique=True, max_length=255, verbose_name='Nom'),
+            field=models.CharField(default=datetime.datetime(2019, 8, 6, 17, 27, 17, 446800), unique=True, max_length=255, verbose_name='Nom'),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='publisher',
             name='name',
-            field=models.CharField(default=b'2019-08-02 19:17:22.008205', unique=True, max_length=255, verbose_name='Nom'),
+            field=models.CharField(default=datetime.datetime(2019, 8, 6, 17, 27, 25, 288528), unique=True, max_length=255, verbose_name='Nom'),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='recordingcontext',
             name='name',
-            field=models.CharField(default=b'2019-08-02 19:17:26.221222', unique=True, max_length=255, verbose_name='Nom'),
+            field=models.CharField(default=datetime.datetime(2019, 8, 6, 17, 27, 33, 953846), unique=True, max_length=255, verbose_name='Nom'),
             preserve_default=False,
         ),
         migrations.AlterField(
             model_name='acquisitionmode',
             name='notes',
             field=models.TextField(null=True, verbose_name='Notes', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='collection',
+            name='code',
+            field=models.CharField(max_length=255, verbose_name='cote', validators=[django.core.validators.RegexValidator(regex=b'^[A-Z]{4}_[A-Z]{3}_[A-Z0-9]{4}_[0-9]{4}$', message=b'Code must conform to XXXX_XXX_000X_0000', code=b'invalid_code')]),
+        ),
+        migrations.AlterField(
+            model_name='collection',
+            name='description',
+            field=models.TextField(null=True, verbose_name='description', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='collection',
+            name='title',
+            field=models.CharField(max_length=255, verbose_name='titre'),
         ),
         migrations.AlterField(
             model_name='coupe',
