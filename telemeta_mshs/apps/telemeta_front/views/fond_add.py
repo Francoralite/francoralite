3dd53ff259a2977085bef4ed342d649521a2bcb4
lluc <leger.luc@gmail.com>
2019-12-08 08:04:34 +0100

    issue #46 [fond] FIX: create

--- telemeta_mshs/apps/telemeta_front/views/fond_add.py
+++ telemeta_mshs/apps/telemeta_front/views/fond_add.py
@@ -6,10 +6,7 @@
 
 
 from django.views.generic.edit import FormView
-from rest_framework import status
 from telemeta_front.forms.fond import FondForm
-import requests
-from settings import FRONT_HOST_URL
 import telemeta_front.tools as tools
 
 
@@ -21,12 +18,7 @@ class FondAdd(FormView):
     def get_initial(self):
         initial = super(FondAdd, self).get_initial()
         initial['institution'] = self.kwargs['id_institution']
-        # Obtain code of the institution
-        response = requests.get(
-            FRONT_HOST_URL + '/api/institution/'
-            + self.kwargs['id_institution'])
-        if response.status_code == status.HTTP_200_OK:
-            initial['code'] = response.json()['code']
+
         return initial
 
     def post(self, request, *args, **kwargs):
