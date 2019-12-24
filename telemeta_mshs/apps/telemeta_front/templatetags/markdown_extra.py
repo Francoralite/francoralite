dca5048117c203f3f47914276cacd5a94576e22f
lluc <leger.luc@gmail.com>
2019-12-24 10:08:47 +0100

    issue #50 #72 #73 #75 [front][text_editor] change text editor and avoid vue.js block

--- telemeta_mshs/apps/telemeta_front/templatetags/markdown_extra.py
+++ telemeta_mshs/apps/telemeta_front/templatetags/markdown_extra.py
@@ -18,28 +18,8 @@ def markdown(value):
     return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
 
 
-@register.simple_tag
-def markdown_editor(component):
-    code = """
-    <script type="text/javascript">
-      var simplemde = new SimpleMDE({
-         element: $("#""" + component + """")[0] ,
-         toolbar: ["bold", "italic", "heading", "|",
-         {
-            name: "custom",
-            action: function customFunction(editor){
-            // Add your own code
-                var cm = editor.codemirror;
-                var doc = cm.getDoc();
-                var cursor = doc.getCursor();
-                doc.replaceRange('  \\r\\n', cursor);
-            },
-            className: "fa fa-paragraph",
-            title: "New line/paragraph",
-            },
-          "quote","unordered-list", "ordered-list", "|","preview", "guide"],
-       });
-    </script>
-    """
-
-    return code
+@register.inclusion_tag('inc/markdown_editor.html')
+def markdown_editor(*args, **kwargs):
+    return{
+        'component': kwargs['component'],
+    }
