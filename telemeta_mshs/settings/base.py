c7c6a0c295e36b9789ed5604c8da9406b35160da
Alexandre Chaussier <a.chaussier@infopen.pro>
2021-03-24 09:36:56 +0100

    [migration-p3][django] Update Django toolbar settings (WIP)

--- telemeta_mshs/settings/base.py
+++ telemeta_mshs/settings/base.py
@@ -255,24 +255,6 @@ if os.getenv('DEBUG').lower() == "true":
     INSTALLED_APPS += ('debug_toolbar',)
     MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
     DEBUG_TOOLBAR_CONFIG = {
-        'SHOW_TOOLBAR_CALLBACK': lambda x: True
-    }
-    DEBUG_TOOLBAR_PATCH_SETTINGS = False
-    DEBUG_TOOLBAR_PANELS = [
-        'debug_toolbar.panels.versions.VersionsPanel',
-        'debug_toolbar.panels.timer.TimerPanel',
-        'debug_toolbar.panels.settings.SettingsPanel',
-        'debug_toolbar.panels.headers.HeadersPanel',
-        'debug_toolbar.panels.request.RequestPanel',
-        'debug_toolbar.panels.sql.SQLPanel',
-        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-        'debug_toolbar.panels.templates.TemplatesPanel',
-        'debug_toolbar.panels.cache.CachePanel',
-        'debug_toolbar.panels.signals.SignalsPanel',
-        'debug_toolbar.panels.logging.LoggingPanel',
-        'debug_toolbar.panels.redirects.RedirectsPanel',
-    ]
-    DEBUG_TOOLBAR_CONFIG = {
-        'JQUERY_URL': '/static/jquery/dist/jquery.min.js',
+        'SHOW_TOOLBAR_CALLBACK': lambda _request: DEBUG
     }
     INTERNAL_IPS = ['127.0.0.1', '0.0.0.0', '172.17.0.1']
