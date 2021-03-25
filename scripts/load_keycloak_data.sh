e534d0a7c1316ed962d86d9af1f2913cf468e16d
Alexandre Chaussier <a.chaussier@infopen.pro>
2021-03-26 00:05:52 +0100

    [migration-p3][docker] load_keycloak_data.sh get current folder to build resource name

--- scripts/load_keycloak_data.sh
+++ scripts/load_keycloak_data.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 data_file_path=test_data/dump_keycloak.sql
+current_folder=$(basename $PWD)
 
 if [ "$1" == "data" ]
 then
@@ -11,9 +12,9 @@ docker-compose stop keycloak \
     && docker-compose rm -f keycloak \
     && docker-compose stop keycloak_db \
     && docker-compose rm -f keycloak_db \
-    && docker volume rm -f telemeta-integration_keycloak_db \
+    && docker volume rm -f "${current_folder}_keycloak_db" \
     && docker-compose up --no-start \
     && docker-compose start keycloak_db \
-    && docker cp "${data_file_path}" telemeta-integration_keycloak_db_1:/tmp/keycloak_data.sql \
+    && docker cp "${data_file_path}" "${current_folder}_keycloak_db_1:/tmp/keycloak_data.sql" \
     && docker-compose exec keycloak_db bash -c "psql -U keycloak -W keycloak < '/tmp/keycloak_data.sql'" \
     && docker-compose up -d
