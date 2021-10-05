b11933e50eaa4cb300a702e12a3b4579e3ac92cd
lluc <leger.luc@gmail.com>
2021-10-05 14:07:39 +0200

    Codecov (#217)
    * [CI] initial Gihub Action workflow script
    
    * Update github_action.yml
    
    * Update github_action.yml
    
    * Update github_action.yml
    
    * Update github_action.yml
    
    * Update github_action.yml
    
    * [ci] Load Keycloack parameters
    
    * [ci] Run a script
    
    * [ci] Run scripts
    
    * [ci] run scripts
    
    * [ci] Calls Coverall service
    
    * [ci] FIX call a script
    
    * [ci] FIX Coveralls token
    
    * [ci] FIX Coveralls token
    
    * [ci] lcov.info
    
    * [ci] success/failure
    
    * [ci] Test path for lcov.info file
    
    * [ci] Using Python 3.7
    
    * [ci] COVERALLS_REPO_TOKEN
    
    * [ci] Comments Coveralls
    
    * [ci] Coveralls with convert lcov
    
    * [ci] Fix : wrong path tests
    
    * [ci] Python v3.8.12  coverage coveragepy-ldoc
    
    * [ci] Setup Python 3.8
    
    * [ci] Vesions numbers for coverage and coveragepy-lcov
    
    * [ci] Version of coverage
    
    * [ci] Create lcov.info
    
    * [ci] Call Coveralls service
    
    * [ci] Remove coveralls service call
    
    * [ci] Add --cov coveralls to py.test
    
    * [ci] coveralls-python
    
    * [ci] Add GITHUB_TOKEN to the app container
    
    * [ci] Test with COVERALLS_REPO_TOKEN
    
    * [ci] Token Coveralls
    
    * [ci] Cancel coveralls-python
    
    * {ci] COVERALLS_REPO_TOKEN
    
    * [ci] GITHUB_TOKEN
    
    * [ci] .coverage
    
    * [ci] coveralls
    
    * [ci] Coveralls from app container
    
    * [ci] fix.
    
    * [ci] Remove coveralls
    
    * [ci] init Codecov
    
    * [ci] Codecov : coverage

--- setup.py
+++ setup.py
@@ -85,6 +85,8 @@ setup(
       'pytest-env==0.6.2',
       'pytest-pythonpath==0.7.3',
       'coveralls==3.0.1',
+      'coverage==5.5',
+      'coveragepy-lcov==0.1.1'
     ],
   },
   platforms = ['OS Independent'],
