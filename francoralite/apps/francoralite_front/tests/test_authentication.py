ba8d08900ad013b3491f82c8736dc05d7bfac698
lluc <leger.luc@gmail.com>
2021-10-26 18:44:51 +0200

    [keycloak] #210 Fonctionnment correct de la redirection du logout

--- francoralite/apps/francoralite_front/tests/test_authentication.py
+++ francoralite/apps/francoralite_front/tests/test_authentication.py
@@ -14,3 +14,20 @@ def test_login(francoralite_selenium_context):
     assert link_logout.text == _("Déconnexion")
 
     #browser.save_screenshot('./login.png')
+
+def test_logout(francoralite_selenium_context):
+    # Go to the home page, with authentication
+    browser = francoralite_selenium_context.homepage(auth=True)
+
+    # Test username link text
+    link_user = browser.find_element(By.XPATH, "//a[contains(@class, 'login')]")
+    assert link_user.text == "contributeur"
+
+    # Test logout link text
+    link_logout = browser.find_element(By.XPATH, "//a[contains(@class, 'logout')]")
+    assert link_logout.text == _("Déconnexion")
+
+    link_logout.click()
+
+    link_page = browser.find_element(By.XPATH, '//a[text()="' + _("Se connecter") + '"]')
+    assert link_page.text == _("Se connecter")
