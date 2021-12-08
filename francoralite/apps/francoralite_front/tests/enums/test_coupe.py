c4b3ac2fb5a0dbdc9cf5a03386da156aa7aaab14
lluc <leger.luc@gmail.com>
2021-12-08 10:42:23 +0100

    [tests][front] #222  Classe de test pour les enums.

--- francoralite/apps/francoralite_front/tests/enums/test_coupe.py
+++ francoralite/apps/francoralite_front/tests/enums/test_coupe.py
@@ -1,29 +1,9 @@
-from enums_test import enums_test
+from enums_test import EnumsTest
 
-tests = enums_test(
-    entity = 'coupe',
+
+class TestCoupe(EnumsTest):
+    entity = 'coupe'
     data = [
-        {"id":"1", "name":"AABB", "notes":"AABB"},
-    ],
-    new_data = {"name":"AAB", "notes":"AAB"},
-)
-
-
-def test_coupe_list(francoralite_context):
-    tests.list_test(context=francoralite_context)
-
-    
-def test_coupe_detail(francoralite_context):
-    tests.details_test(context=francoralite_context)
-    
-    
-def test_coupe_add(francoralite_context):
-    tests.add_test(context=francoralite_context)
-    
-    
-def test_coupe_update(francoralite_context):
-    tests.update_test(context=francoralite_context)
-
-
-def test_coupe_err_409(francoralite_context):
-    tests.err_409_test(context=francoralite_context)
+        {"id":"1", "name":"AABB", "notes":"Notes de AABB"},
+    ]
+    new_data = {"name":"AAB", "notes":"Notes de AAB"}
