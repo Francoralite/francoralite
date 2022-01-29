6491940ed217c4ef686d20301b0253acb52f4835
lluc <leger.luc@gmail.com>
2022-01-29 10:19:08 +0100

    [back][search] #161 FIX : Recherche multi-critères : domain (genre) musique,

--- francoralite/apps/francoralite_front/tests/enums/test_domain_music.py
+++ francoralite/apps/francoralite_front/tests/enums/test_domain_music.py
@@ -6,6 +6,7 @@ class TestDomainMusic(EnumsTest):
     entity = 'domain_music'
     title = _('Genre de musique')
     data = [
-        {"id":"1", "name":"air de musique", "notes":"Notes air de musique"}
+        {"id":"1", "name":"air de musique", "notes":"Notes air de musique"},
+        {"id":"2", "name":"air de chanson", "notes":"Notes air de chanson"}
     ]
     new_data = {"name":"air de fanfare", "notes":"Notes air de fanfare"}
