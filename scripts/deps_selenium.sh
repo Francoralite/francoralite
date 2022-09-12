da66f910268d4f2a81c34e6f273d7a035eca2f72
lluc <leger.luc@gmail.com>
2022-09-12 10:59:54 +0200

    [test][bug] FIX : nouvelle version de firefox-esr (firefox-esr=91.13.0esr-1~deb10u1)

--- scripts/deps_selenium.sh
+++ scripts/deps_selenium.sh
@@ -3,13 +3,13 @@
 apt update
 
 # Install web browser Firefox
-apt install -y firefox-esr=78.14.0esr-1~deb10u1
+apt install -y firefox-esr=91.13.0esr-1~deb10u1
 
 # Install wget
 apt install wget
 
 # Install Gecko driver
-export GECKO_DRIVER_VERSION='v0.30.0'
+export GECKO_DRIVER_VERSION='v0.31.0'
 wget https://github.com/mozilla/geckodriver/releases/download/$GECKO_DRIVER_VERSION/geckodriver-$GECKO_DRIVER_VERSION-linux64.tar.gz
 tar -xvzf geckodriver-$GECKO_DRIVER_VERSION-linux64.tar.gz
 rm geckodriver-$GECKO_DRIVER_VERSION-linux64.tar.gz
