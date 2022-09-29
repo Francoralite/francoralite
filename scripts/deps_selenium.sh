1b55fefaffa863011a005068bb24f806b29d861d
Fabien <fab2713+github@gmail.com>
2022-09-29 16:22:36 +0200

    [test][bug] FIX : retour arrière sur la version de firefox-esr et activation d’un dépôt snapshot

--- scripts/deps_selenium.sh
+++ scripts/deps_selenium.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Reactivation of the main buster depot via snapshot.debian.org for firefox-esr 78.14
+sed -e '/snapshot.* buster /s/^# //' -i /etc/apt/sources.list
+
 apt update
 
 # Install web browser Firefox
-apt install -y firefox-esr=91.13.0esr-1~deb10u1
+apt install -y firefox-esr=78.14.0esr-1~deb10u1
 
 # Install wget
 apt install wget
 
 # Install Gecko driver
-export GECKO_DRIVER_VERSION='v0.31.0'
+export GECKO_DRIVER_VERSION='v0.30.0'
 wget https://github.com/mozilla/geckodriver/releases/download/$GECKO_DRIVER_VERSION/geckodriver-$GECKO_DRIVER_VERSION-linux64.tar.gz
 tar -xvzf geckodriver-$GECKO_DRIVER_VERSION-linux64.tar.gz
 rm geckodriver-$GECKO_DRIVER_VERSION-linux64.tar.gz
