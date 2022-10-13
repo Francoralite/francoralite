81b3c95c2fc7181c18001b54948a4a3001ec25f0
Fabien <fab2713+github@gmail.com>
2022-10-13 20:28:13 +0200

    [test][front] amélioration des tests sur les énumérations en vue de l’intégration des blocs de la page d’accueil

--- francoralite/apps/francoralite_front/tests/enums/test_hornbostelsachs.py
+++ francoralite/apps/francoralite_front/tests/enums/test_hornbostelsachs.py
@@ -4,23 +4,10 @@ from enums_test import EnumsTest
 
 class TestHornbostelsachs(EnumsTest):
     entity = 'hornbostelsachs'
+    first_text_field = 'number'
+    second_text_field = 'name'
     title = _("Classification Hornbostel-Sachs")
     data = [
         {"id":"1", "number":"321.322", "name":"Violon"}
     ]
     new_data = {"number":"4", "name":"Voix"}
-    
-    def test_list(self, francoralite_context, name="number"):
-        super().test_list(francoralite_context, name="number")
-        
-    def test_details(self, francoralite_context, name="number", notes="name"):
-        super().test_details(francoralite_context, name="number", notes="name")
-        
-    def test_add(self, francoralite_context, name="number", notes="name"):
-        super().test_add(francoralite_context, name="number", notes="name")
-        
-    def test_update(self, francoralite_context, notes="name"):
-        super().test_update(francoralite_context, notes="name")
-        
-    def test_create_err_409(self, francoralite_context, name="number"):
-        super().test_create_err_409(francoralite_context, name="number")
