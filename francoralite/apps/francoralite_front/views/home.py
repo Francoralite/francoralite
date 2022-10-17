502b86b3d097b5353a3fc059fd57e1134c7ad306
Fabien <fab2713+github@gmail.com>
2022-10-17 16:17:54 +0200

    [front][block] #300 utilisation des blocs personnalisés sur la page d’accueil

--- francoralite/apps/francoralite_front/views/home.py
+++ francoralite/apps/francoralite_front/views/home.py
@@ -4,9 +4,20 @@
 #
 # Authors: Luc LEGER / Coopérative ARTEFACTS <artefacts.lle@gmail.com>
 
+from francoralite.apps.francoralite_front import tools
 from francoralite.apps.francoralite_front.francoralite_template_view import FrancoraliteTemplateView
 
 
 class HomePageView(FrancoraliteTemplateView):
 
     template_name = "../templates/home.html"
+
+    def get_context_data(self, **kwargs):
+        try:
+            context = super(HomePageView, self).get_context_data(**kwargs)
+            context['blocks'] = tools.request_api('/api/block?show=true')
+        except Exception as err:
+            context['blocks'] = []
+            context['error'] = err
+
+        return context
