649accffd87d3850b18789f5a0bfa91aa95716da
Fabien <fab2713+github@gmail.com>
2023-11-22 17:27:35 +0100

    [front][institution] #380 ajout des tris sur la liste et le détail des institutions

--- francoralite/apps/francoralite_api/views/institution.py
+++ francoralite/apps/francoralite_api/views/institution.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from ..models.institution import Institution as InstitutionModel
 from ..serializers.institution import InstitutionSerializer
 
@@ -10,6 +10,9 @@ class InstitutionViewSet(viewsets.ModelViewSet):
 
     queryset = InstitutionModel.objects.all()
     serializer_class = InstitutionSerializer
+
+    filter_backends = (filters.OrderingFilter,)
+    ordering = ('name',)
 
     keycloak_scopes = {
         'GET': 'institution:view',
