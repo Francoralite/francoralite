885bb0f1e8f5a092400ae440fe371ce458d20435
lluc <leger.luc@gmail.com>
2023-11-30 17:25:11 +0100

    [front][base] #388 affichage version/tag et date

--- francoralite/apps/francoralite_front/templatetags/tags.py
+++ francoralite/apps/francoralite_front/templatetags/tags.py
@@ -16,13 +16,47 @@ try:
 except:
     VERSION = "inconnu"
 
-hostname = os.getenv('HOSTNAME')
+# --- GIT Label---
+# Use Popen for running git commands and capturing output
+# Get the latest commit hash that was tagged
+get_latest_tagged_commit = ["git", "rev-list", "--tags", "--max-count=1"]
+latest_tagged_commit = (
+    subprocess.check_output(get_latest_tagged_commit).decode().strip()
+)
 
+with subprocess.Popen(
+    [
+        "git",
+        "describe",
+        "--tags",
+        latest_tagged_commit,
+        "--abbrev=0",
+    ],
+    stdout=subprocess.PIPE,
+) as proc:
+    last_tag = proc.stdout.read().decode().strip()
+
+with subprocess.Popen(
+    ["git", "show", "-s", "--format=%ai"], stdout=subprocess.PIPE
+) as proc:
+    last_tag_date = proc.stdout.read().strip().decode()[:10]
+
+# Print the tag and its date
+LABEL = last_tag
+LABEL_DATE = last_tag_date
+
+
+hostname = os.getenv("HOSTNAME")
 
 
 @register.simple_tag()
 def git_short_version():
     return VERSION
+
+
+@register.simple_tag()
+def git_label():
+    return f"{LABEL} ({LABEL_DATE})"
 
 
 @register.simple_tag()
