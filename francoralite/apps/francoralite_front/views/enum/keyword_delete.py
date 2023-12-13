c63548c3b5e56a72b67ea951ddaa05f7e7927180
Fabien <fab2713+github@gmail.com>
2023-12-13 13:23:34 +0100

    [front][keyword] #393 ajout de la liste et des formulaires de gestion des mots-clés

---
+++ francoralite/apps/francoralite_front/views/enum/keyword_delete.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Luc LEGER / Coopérative ARTEFACTS <artefacts.lle@gmail.com>
+
+from django.views.generic.base import View
+from ... import tools as tools
+
+
+class KeywordDelete(View):
+    keycloak_scopes = {
+        'DEFAULT': 'keyword:delete',
+    }
+    def get(self, request, *args, **kwargs):
+        return tools.delete('keyword', request, *args, **kwargs)
