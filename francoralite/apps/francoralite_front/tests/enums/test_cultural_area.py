976ce5b988422decc35b4d8bf73f41e39c1c1610
lluc <leger.luc@gmail.com>
2024-04-19 17:32:01 +0200

    [front][cultural_area] FIX tests

--- francoralite/apps/francoralite_front/tests/enums/test_cultural_area.py
+++ francoralite/apps/francoralite_front/tests/enums/test_cultural_area.py
@@ -1,7 +1,7 @@
 from enums_test import EnumsTest
 
 
-class TestCivility(EnumsTest):
+class TestCulturalArea(EnumsTest):
     second_text_field = 'geojson'
     second_text_value = '{"foo": "bar", "spam": 42}'
     clear_fields_on_add = True
@@ -11,9 +11,9 @@ class TestCivility(EnumsTest):
         {"id":"1", "name":"Poitou", "geojson":'{"type": "Polygon", '
             '"coordinates": [[[1.021729, 46.562637], [0.587769, 46.984], '
             '[-0.22522, 47.06638], [-2.312622, 47.017716], '
-            '[-1.653442, 46.327965], [-0.488892, 46.297611], '
-            '[0.447006, 45.926319], [1.021729, 46.562637]]]}'},
+            '[-1.653442, 46.327965], ...'},
         {"id":"2", "name":"Saintonge, Poitou", "geojson":"null"},
         {"id":"3", "name":"Vendée", "geojson":"null"},
     ]
     new_data = {"name":"Québec", "geojson":'{"key": "value", "number": 42}'}
+
